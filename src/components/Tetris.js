0ff5bed4882e71fffe71330625d474f1988e8d14
abidurr <42abid@gmail.com>
2020-05-05 16:40:31 -0400

    working on useStage and usePlayer

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { createStage } from "../gameHelpers"
 // styled components
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris"
 // custom hooks
@@ -13,11 +14,45 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
 
-    const [player] = usePlayer()
+    const [player, updatePlayerPos, resetPlayer] = usePlayer()
     const [stage, setStage] = useStage(player)
 
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y: 0 })
+    }
+    const startGame = () => {
+        // reset everything
+        setStage(createStage())
+        resetPlayer()
+    }
+    const drop = () => {
+        updatePlayerPos({
+            x: 0,
+            y: 1, 
+            collided: false
+        })
+    }
+    const dropPlayer = () => {
+        drop()
+    }
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1)
+            } else if (keyCode === 39) {
+                movePlayer(1)
+            } else if (keyCode === 40) {
+                dropPlayer()
+            }
+        }
+    }
+
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper
+            role="button"
+            tabIndex="0"
+            onKeyDown={e => move(e)}
+        >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
@@ -33,7 +68,9 @@ const Tetris = () => {
                         <Display text="Levels" />
                     </div>
                     )}
-                    <StartButton />
+                    <StartButton 
+                        onClick={startGame}
+                    />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
